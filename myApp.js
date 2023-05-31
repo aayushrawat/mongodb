d1035db8aa7f72c1fa2cb31903ca54b80994886e
aayushrawat <aayushrawatmain@gmail.com>
2023-06-01 01:24:36 +0530

    createmanypeople

--- myApp.js
+++ myApp.js
@@ -36,8 +36,35 @@ const createAndSavePerson = (done) => {
   });
 };
 
+
+
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  
+  const arrayOfPeople = [
+    {
+      name: 'John',
+      age: 30,
+      favoriteFoods: ['Pizza', 'Burger']
+    },
+    {
+      name: 'Emily',
+      age: 25,
+      favoriteFoods: ['Sushi', 'Ice Cream']
+    },
+    {
+      name: 'Michael',
+      age: 40,
+      favoriteFoods: ['Steak', 'Pasta', 'Chocolate']
+    }
+  ];
+  Person.create(arrayOfPeople, function(err, createdPeople) {
+    if (err) {
+      console.error(err);
+      done(err);
+    } else {
+      done(null, createdPeople);
+    }
+  });
 };
 
 const findPeopleByName = (personName, done) => {
